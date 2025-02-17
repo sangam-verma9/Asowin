eb9f317768fda611c90daa9cbc6712e84fbf4f0a
sangam verma <sangamverma955911@gmail.com>
2025-02-17 17:16:27 +0530

    choose asowinchange

--- src/app/components/WhyChooseSection.js
+++ src/app/components/WhyChooseSection.js
@@ -34,11 +34,11 @@ const WhyChooseSection = () => {
   ];
 
   return (
-    <div className="relative w-full min-h-screen mb-[50px]">
+    <div className="relative w-full mb-[50px]">
       {/* Background Image */}
       <div
         className="absolute inset-0 w-full h-full bg-center bg-cover bg-no-repeat"
-        style={{ backgroundImage: "url('/whyus.png')" }} 
+        style={{ backgroundImage: "url('/whyus.png')" }}
       >
         <div className="absolute inset-0"></div>
       </div>
@@ -54,7 +54,7 @@ const WhyChooseSection = () => {
             <p className="text-base md:text-lg max-w-2xl mx-auto text-gray-600">
               We offer advanced AI-focused App Optimization solutions to help your app boost core metrics in the most advanced way.
             </p>
-          </div> 
+          </div>
 
 
           {/* Features Grid */}
